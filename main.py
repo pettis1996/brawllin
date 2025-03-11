368943a15eca008d537e8e16a17bfbb8cf6e6ded
Paris Pettis <pettis_paris@hotmail.com>
2025-03-11 06:11:28 +0200

    added fighter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from fighter import Fighter
 
 pygame.init()
 
@@ -13,10 +14,16 @@ scaled_bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 def draw_bg():
     screen.blit(scaled_bg_image, (0, 0))
+    
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
 
 game_running = True
 while game_running:
     draw_bg()
+    
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
