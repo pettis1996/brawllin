79ef8d4ad8ae8e0448c04689cc83ca2c87a23c77
Paris Pettis <pettis_paris@hotmail.com>
2025-03-11 07:44:11 +0200

    fixed attacking and added health bars to the players

--- fighter.py
+++ fighter.py
@@ -2,28 +2,74 @@ import pygame
 
 class Fighter:
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
         
-    def move(self, screen_width):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
+        GRAVITY = 2
+        
         dx = 0
         dy = 0
         
         key = pygame.key.get_pressed()
         
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-            
+        # Cannot perform movement when attacking
+        if not self.attacking:
+            # Movement on X axis
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+                
+            # Movement on Y axis - Jump
+            if key[pygame.K_w] and not self.jump:
+                self.vel_y = -30
+                self.jump = True
+                
+            # Attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
+        
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+         
+        # Avoid edges X and Y   
         if self.rect.left + dx < 0:
             dx = -self.rect.left
-        
         if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 110:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 110 - self.rect.bottom
+        
+        # The side that the players are facing
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
         
         self.rect.x += dx
         self.rect.y += dy
+    
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+        
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+       
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
--- main.py
+++ main.py
@@ -12,11 +12,22 @@ pygame.display.set_caption("Brawllin - Just a Dev")
 clock = pygame.time.Clock()
 FPS = 60
 
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 bg_image = pygame.image.load("assets/images/background/bg-china.jpg")
 scaled_bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def draw_bg():
     screen.blit(scaled_bg_image, (0, 0))
+    
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
     
 fighter_1 = Fighter(200, 310)
 fighter_2 = Fighter(700, 310)
@@ -27,8 +38,11 @@ while game_running:
     
     draw_bg()
     
-    fighter_1.move(SCREEN_WIDTH)
-    fighter_2.move(SCREEN_WIDTH)
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+    
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
     
     fighter_1.draw(screen)
     fighter_2.draw(screen)
