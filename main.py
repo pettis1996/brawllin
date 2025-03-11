aa5d613fde4a7edd604bb38bb94bfe389f240775
Paris Pettis <pettis_paris@hotmail.com>
2025-03-11 06:20:27 +0200

    adding movement and setting fps cap

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawllin - Just a Dev")
 
+clock = pygame.time.Clock()
+FPS = 60
+
 bg_image = pygame.image.load("assets/images/background/bg-china.jpg")
 scaled_bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -20,7 +23,12 @@ fighter_2 = Fighter(700, 310)
 
 game_running = True
 while game_running:
+    clock.tick(FPS)
+    
     draw_bg()
+    
+    fighter_1.move()
+    fighter_2.move()
     
     fighter_1.draw(screen)
     fighter_2.draw(screen)
