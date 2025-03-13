cc0c35cad9bd898f2f4f5d6d576dade5a7d11684
Paris Pettis <pettis_paris@hotmail.com>
2025-03-13 07:11:44 +0200

    added countdown timer for the fight and added new font

--- main.py
+++ main.py
@@ -3,28 +3,41 @@ from fighter import Fighter
 
 pygame.init()
 
+# Window Dimensions
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
+# Setting up window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawllin - Just a Dev")
 
 clock = pygame.time.Clock()
 FPS = 60
 
+# Colors
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
+# Game Variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+fight_text_count = 50
+last_fight_text_count = pygame.time.get_ticks()
+
+# Warrior Variables
 WARRIOR_SIZE = 162
 WARRIOR_SCALE = 4
 WARRIOR_OFFSET = [72, 56]
 WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+
+# Wizard Variables
 WIZARD_SIZE = 250
 WIZARD_SCALE = 3
 WIZARD_OFFSET = [112, 107]
 WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
+# Load the background
 bg_image = pygame.image.load("assets/images/background/bg-china.jpg").convert_alpha()
 scaled_bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -35,6 +48,14 @@ wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").conv
 # Number of steps in each animation
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+# Fonts
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 def draw_bg():
     screen.blit(scaled_bg_image, (0, 0))
@@ -58,8 +79,20 @@ while game_running:
     draw_health_bar(fighter_1.health, 20, 20)
     draw_health_bar(fighter_2.health, 580, 20)
     
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
-    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+    if intro_count <= 0:
+        if fight_text_count >= 0:
+            if (pygame.time.get_ticks() - last_fight_text_count) >= 1:
+                fight_text_count -= 1
+                last_fight_text_count = pygame.time.get_ticks()
+                draw_text("FIGHT", count_font, RED, SCREEN_WIDTH / 3 + 80, SCREEN_HEIGHT / 3)
+        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+        fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+    else:
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+        if (pygame.time.get_ticks() - last_count_update) >= 1000:
+            intro_count -= 1
+            last_count_update = pygame.time.get_ticks()
+            draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
     
     fighter_1.update()
     fighter_2.update()
